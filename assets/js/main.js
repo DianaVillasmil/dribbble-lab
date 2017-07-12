379199c34e86628f9c5d3348f019f2c959bec4cf
Diana Villasmil <diacavipi@gmail.com>
2017-07-12 00:45:55 -0400

    row y col resuelto

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 		console.log(resp);
 		if(resp.data.length > 0){
 			$.each(resp.data.reverse(), function(i, val){
-				$("#dribbble-shots").prepend('<li class="list-group-item"><div class="thumbnail"><img src="' + val.images.normal + '" alt="..."><div class="caption"><span><i class="fa fa-link" aria-hidden="true"></i></span><ul><li><i class="fa fa-eye" aria-hidden="true"></i>' + val.views_count + '</li><li><i class="fa fa-comment-o" aria-hidden="true"></i>' + val.comments_count + '</li><li><i class="fa fa-heart" aria-hidden="true"></i>' + val.likes_count + '</li></ul></div></div></li>');
+				$("#dribbble-shots").prepend('<div class="col-md-4 col-xs-6 pad20"><li class="list-group-item"><div class="thumbnail"><img src="' + val.images.normal + '" alt="..."><div class="caption"><span><i class="fa fa-link" aria-hidden="true"></i></span><ul><li><i class="fa fa-eye" aria-hidden="true"></i>' + val.views_count + '</li><li><i class="fa fa-comment-o" aria-hidden="true"></i>' + val.comments_count + '</li><li><i class="fa fa-heart" aria-hidden="true"></i>' + val.likes_count + '</li></ul></div></div></li></div>');
 			});
 		}else{
 			$("#dribbble-shots").append("<li>No shots.</li>");
